bb3cf0fd1a8fb0d10c6e1ec148ad7a532216b53b
Dipak <sinhadipak33@gmail.com>
2024-04-07 15:48:00 -0400

    code refactoring

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.use(
 );
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
 
 // Create Users and Attendance table schemas
 db.serialize(() => {
@@ -660,7 +660,7 @@ app.get("/upload", async (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
